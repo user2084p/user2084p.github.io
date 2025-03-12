99266bd811c2ba1f90650f0bb9b5282c8df99aaa
user2084p <user_2084@protonmail.com>
2025-03-12 02:54:33 -0600

    .

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/rjshkhr/cleanfolio'
+      className='link footer__link'
+    >
+      user2084 portfolio 
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,74 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: 'https://user2084p.github.io/',
+  title: 'JS.',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'User_2084',
+  role: 'Individuo',
+  description:
+    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+  resume: 'https://example.com',
+  social: {
+    linkedin: 'https://linkedin.com',
+    github: 'https://https://user2084p.github.io/.com',
+  },
+}
+
+const projects = [
+  // projects can be added an removed
+  // if there are no projects, Projects section won't show up
+  {
+    name: 'Proyecto 1',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Proyecto 2',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Proyecto 3',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Proyecto 4',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+]
+
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+  'QGis',
+  'ArcGis',
+  'Python',
+  'Earth Engine',
+  'Imagenes Satelitales',
+  'Git',
+  'Redux',
+]
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: 'user2084@mail.com',
+}
+
+export { header, about, projects, skills, contact }
